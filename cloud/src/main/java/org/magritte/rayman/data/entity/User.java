1b3f0149a776b6f6beb090e658467b4364ffbbae
Santiago <santi.casabo0026@gmail.com>
2020-05-30 16:31:09 -0300

    Implementacion del login

--- cloud/src/main/java/org/magritte/rayman/data/entity/User.java
+++ cloud/src/main/java/org/magritte/rayman/data/entity/User.java
@@ -23,15 +23,19 @@ public class User {
     private String name;
     private String lastname;
     private String password;
+    private String email;
+    private char clase; //Tipo de usuario(medico = m o paciente = p)
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Exercise> exercise;
 
-    public User(String dni, String name, String lastname, String password){
+    public User(String dni, String name, String lastname, String password, String email, char c){
         this.dni = dni;
         this.name = name;
         this.lastname = lastname;
         this.password = password;
+        this.email = email;
+        this.clase = c;
     }
 }
 
