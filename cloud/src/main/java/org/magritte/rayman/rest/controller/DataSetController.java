7a5866c504e9745bf63d730b210a660c18403f9b
JCatrielLopez <jlopez@alumnos.exa.unicen.edu.ar>
2020-06-26 17:06:22 -0300

    Bueno, habia cerrado el pull request para poder mejorar unas cosas. Despues de pensarlo un poco, el formato no me gustaba. Ahora lo separe en tres metodos, para poder:
    -
    - Obtener las estadisticas de un usuario filtrado por una unidad (bpm, presion, lo que sea).
    - Obtener las estadisticas de una rutina, filtrado por unidad.
    - Obtener las estadisticas de todos los pacientes de un medico, filtrando por unidad.
    - Obtener las estadisticas globales (todos los pacientes), filtrando por unidad.
    
    O sea que ahora no solo podriamos comparar un paciente con todos (sin distinguir la rutina), sino que podriamos comparar un paciente y sus estadisticas en esa misma rutina que esta haciendo.

--- cloud/src/main/java/org/magritte/rayman/rest/controller/DataSetController.java
+++ cloud/src/main/java/org/magritte/rayman/rest/controller/DataSetController.java
@@ -1,13 +1,11 @@
 package org.magritte.rayman.rest.controller;
 
+import org.apache.commons.math3.stat.descriptive.summary.Sum;
 import org.magritte.rayman.data.entity.DataSet;
-import org.magritte.rayman.data.entity.Medic;
 import org.magritte.rayman.data.entity.Patient;
 import org.magritte.rayman.data.entity.Routine;
 import org.magritte.rayman.rest.request.DataSetRequest;
-import org.magritte.rayman.rest.response.DataSetResponse;
-import org.magritte.rayman.rest.response.PatientResponse;
-import org.magritte.rayman.rest.response.StatsResponse;
+import org.magritte.rayman.rest.response.*;
 import org.magritte.rayman.service.DataSetService;
 import org.magritte.rayman.service.RoutineService;
 import org.magritte.rayman.service.UserService;
@@ -15,14 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.naming.spi.DirObjectFactory;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
+
 import org.apache.commons.math3.stat.StatUtils;
 @RestController
 @RequestMapping(name = "/dataset")
@@ -90,49 +89,77 @@ public class DataSetController {
         return new DataSetResponse(dataSet);
     }
 
+
+    private SummaryResponse getSummary(Collection<DataSet> collection ){
+        SummaryResponse out = new SummaryResponse();
+
+        double[] values_array = collection
+                .stream()
+                .mapToDouble(DataSet::getMeasurement)
+                .toArray();
+
+        out.setAvg(StatUtils.mean(values_array));
+        out.setMax(StatUtils.max(values_array));
+        out.setMin(StatUtils.min(values_array));
+        out.setVariance(StatUtils.percentile(values_array, out.getAvg()));
+
+        return out;
+    }
+
     /**
-     * Get a summary of the patient data filtered by unit
+     * Get a summary of the patient data filtered by unit (all routines)
      *
-     * @return StatsResponse
+     * @return SummaryResponse
      */
-    @GetMapping("/stats/{id}")
+    @GetMapping("/stats/patient/{id}")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.OK)
-    public StatsResponse getStatsByUnit(@PathVariable Integer id, @RequestParam String unit) {
+    public SummaryResponse getPatientSummary(@PathVariable Integer id, @RequestParam String unit) {
 
         Predicate<DataSet> pr = a->(!a.getUnit().equalsIgnoreCase(unit));
         Patient patient = (Patient) userService.getUserById(id);
 
         List<DataSet> patient_dataset = dataSetService.getDataSetByPatient(patient);
-        List<DataSet> global_dataset = dataSetService.getDataSets();
         patient_dataset.removeIf(pr);
-        global_dataset.removeIf(pr);
 
-        double[] global_array = global_dataset
-                .stream()
-                .mapToDouble(DataSet::getMeasurement)
-                .toArray();
+        SummaryResponse sum = this.getSummary(patient_dataset);
+        SummaryPatientResponse out = new SummaryPatientResponse();
 
-        double[] patient_array = patient_dataset
-                .stream()
-                .mapToDouble(DataSet::getMeasurement)
-                .toArray();
-
-
-        StatsResponse out = new StatsResponse();
-
-        out.setPatient(id);
+        out.setIdPatient(id);
         out.setUnit(unit);
-        out.setMean(StatUtils.mean(patient_array));
-        out.setMax(StatUtils.max(patient_array));
-        out.setMin(StatUtils.min(patient_array));
-        out.setVariance(StatUtils.percentile(patient_array, out.getMean()));
+        out.setAvg(sum.getAvg());
+        out.setMax(sum.getMax());
+        out.setMin(sum.getMin());
+        out.setVariance(sum.getVariance());
 
-        out.setGlobal_mean(StatUtils.mean(global_array));
-        out.setGlobal_max(StatUtils.max(global_array));
-        out.setGlobal_min(StatUtils.min(global_array));
-        out.setGlobal_mode(StatUtils.mode(global_array));
-        out.setGlobal_variance(StatUtils.variance(global_array, out.getGlobal_mean()));
+        return out;
+    }
+
+    /**
+     * Get a summary of the patients data filtered by routine
+     *
+     * @return SummaryResponse
+     */
+    @GetMapping("/stats/routine/{id}")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    public SummaryResponse getRoutineSummary(@PathVariable Integer id, @RequestParam String unit) {
+
+        Predicate<DataSet> pr = a->(!a.getUnit().equalsIgnoreCase(unit));
+        Routine routine = routineService.getRoutineById(id);
+
+        Set<DataSet> routine_dataset = routine.getDataSets();
+        routine_dataset.removeIf(pr);
+
+        SummaryResponse sum = this.getSummary(routine_dataset);
+        SummaryRoutineResponse out = new SummaryRoutineResponse();
+
+        out.setIdRoutine(id);
+        out.setUnit(unit);
+        out.setAvg(sum.getAvg());
+        out.setMax(sum.getMax());
+        out.setMin(sum.getMin());
+        out.setVariance(sum.getVariance());
 
         return out;
     }
@@ -140,21 +167,40 @@ public class DataSetController {
     /**
      * Get a summary of all the patients linked to a doctor
      *
-     * @return List of StatsResponse
+     * @return List of SummaryResponse
      */
     @GetMapping("/stats/medic/{id}")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.OK)
-    public HashSet<StatsResponse> getStatsByDoctor(@PathVariable Integer id, @RequestParam String unit) {
+    public HashSet<SummaryResponse> getStatsByDoctor(@PathVariable Integer id, @RequestParam String unit) {
 
         List<PatientResponse> patients = userService.getPatientsFromMedic(id);
-        HashSet<StatsResponse> out = new HashSet<>();
+        HashSet<SummaryResponse> out = new HashSet<>();
 
         for(PatientResponse p: patients){
-            out.add(getStatsByUnit(p.getId(), unit));
+            out.add(getPatientSummary(p.getId(), unit));
         }
 
         return out;
+
+    }
+
+    /**
+     * Get a summary of all the patients
+     *
+     * @return SummaryResponse
+     */
+
+    @GetMapping("/stats/global")
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
+    public SummaryResponse getStatsGlobal(@RequestParam String unit) {
+
+        Predicate<DataSet> pr = a->(!a.getUnit().equalsIgnoreCase(unit));
+        List<DataSet> all_datasets = dataSetService.getDataSets();
+        all_datasets.removeIf(pr);
+
+        return this.getSummary(all_datasets);
 
     }
 
