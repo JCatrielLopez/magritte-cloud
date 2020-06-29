c3f80cff7e2226251cb5fd30a24c039dd81dce29
Santiago <santi.casabo0026@gmail.com>
2020-06-29 15:33:30 -0300

    cambio en simulationResponse

--- cloud/src/main/java/org/magritte/rayman/rest/response/SimulationResponse.java
+++ cloud/src/main/java/org/magritte/rayman/rest/response/SimulationResponse.java
@@ -1,10 +1,9 @@
 package org.magritte.rayman.rest.response;
 
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,17 +14,25 @@ public class SimulationResponse{
 
     private Integer speed;
     private Integer status_code;
-    private Set<DataSetResponse> sensors;
+    private Set<DataSetSimulation> sensors;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    private class DataSetSimulation{
+        private Integer id;
+        private Integer measurement;
+        private String unit;
+    }
 
     public SimulationResponse() {
         this.speed = 5; // TODO Endpoint: /setSpeed
         this.status_code = 2; // TODO
         this.sensors = new HashSet<>();
-        DataSetResponse dr = new DataSetResponse();
+        DataSetSimulation dr = new DataSetSimulation();
         dr.setMeasurement((int) (Math.random() * (0.6 * (211 - 0.64 * 45)) + 60));
         dr.setUnit("bpm");
-        dr.setIdDataSet(1);
-        dr.setDataType("heart rate monitor");
+        dr.setId(1);
         this.sensors.add(dr);
     }
 }
